3799afb50334d6b826d9b96266ef10254d5dc95f
Gaurav Kumar <gk.chadda.dev@gmail.com>
2026-02-18 13:52:15 +0530

    Adding mutex example

---
+++ mutex_example.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+using namespace std;
+
+
+mutex mx;
+int number = 0;
+void increment()
+{
+    mx.lock();
+    for(int i=0;i<1000000;i++)
+    {
+        number++;
+    }
+    mx.unlock();
+}
+
+int main()
+{
+    thread t1(increment);
+    thread t2(increment);
+    
+    t1.join();
+    t2.join();
+    
+    cout<<number;
+    return 0;
+}
